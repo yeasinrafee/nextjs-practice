af7273da6836f3df89e985da09c5a283c5fe13d7
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-04 23:33:36 +0600

    Client only code

--- rendering/src/app/client-route/page.tsx
+++ rendering/src/app/client-route/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import React from 'react';
-
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { useTheme } from '../components/theme-provider';
+import { clientSideFunction } from '@/utils/clients-utils';
 
 export default function ClientRoutePage() {
   const theme = useTheme();
+  const result = clientSideFunction();
   return (
     <div className='image-slider-container'>
       <h1
@@ -16,6 +15,7 @@ export default function ClientRoutePage() {
         }}
       >
         Client Route
+        <h2>{result}</h2>
       </h1>
     </div>
   );
--- rendering/src/app/server-route/page.tsx
+++ rendering/src/app/server-route/page.tsx
@@ -1,8 +1,10 @@
 import { serverSideFunction } from '@/utils/server-utils';
 import ImageSlider from '../components/ImageSlider';
+import { clientSideFunction } from '@/utils/clients-utils';
 
 export default function ServerRoutePage() {
   const result = serverSideFunction();
+  const clientResult = clientSideFunction();
   return (
     <div>
       <h1>ServerRoutePage</h1>
