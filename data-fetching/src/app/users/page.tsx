44565bfbc36b3a23310d0a13b7f326330621f3bc
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-05 00:20:30 +0600

    JSON Server

--- data-fetching/src/app/users/page.tsx
+++ data-fetching/src/app/users/page.tsx
@@ -8,7 +8,7 @@ type User = {
 
 export default async function UsersPage() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const response = await fetch('https://jsonplaceholder.typicode.com/users12');
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await response.json();
   console.log(users);
   return (
