6ce42c15b3400c342cef2c4d371a9bf7e02e16b7
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-04 16:21:08 +0600

    header and cookies

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-  return new Response();
+import { headers, cookies } from 'next/headers';
+import { type NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+
+  cookies().set('resultsPerPage', '20');
+  const theme = request.cookies.get('theme');
+
+  console.log(requestHeaders.get('Authorization'));
+  console.log(headerList.get('Authorization'));
+
+  console.log(theme);
+  console.log(cookies().get('resultsPerPage'));
+
+  return new Response('<h1>Profile API Data</h1>', {
+    headers: {
+      'Content-Type': 'text/html',
+      'Set-Cookie': 'theme=dark',
+    },
+  });
 }
